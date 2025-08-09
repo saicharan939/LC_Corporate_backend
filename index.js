9c99c5e09ccae7ab267effdf577b2b42b0ff19d0
saicharan939 <sangishettysaicharan@gmail.com>
2025-08-09 13:23:49 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: 'https://lc-corporate-frontend.vercel.app/', // Replace with your Vercel URL
+  origin: 'https://lc-corporate-frontend.vercel.app', // Replace with your Vercel URL
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
